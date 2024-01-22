ca889a0fc58a2da752909e3882e5d7b65703e8cd
Kris10 <kmrichie611@gmail.com>
2024-01-22 16:01:21 -0600

    edited about and portfolio components

---
+++ src/components/pages/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+        <div>
+            <h2>About Me</h2>
+            <img src="./assets/crazy-cat.jpeg" alt="profile picture" />
+            <p>I am a full-stack developer!</p>
+
+        </div>
+    )
+}
---
+++ src/components/pages/Portfolio.jsx
@@ -0,0 +1,46 @@
+export default function Portfolio() {
+    return (
+        <div>
+            <div className='proj-container'>
+                <div>
+                    <a href="">
+                        <img src="" alt="" />
+                        <figcaption>Project 1</figcaption>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <img src="" alt="" />
+                        <figcaption>Project 2</figcaption>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <img src="" alt="" />
+                        <figcaption>Project 3</figcaption>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <img src="" alt="" />
+                        <figcaption>Project 4</figcaption>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <img src="" alt="" />
+                        <figcaption>Project 5</figcaption>
+                    </a>
+                </div>
+                <div>
+                    <a href="">
+                        <img src="" alt="" />
+                        <figcaption>Project 6</figcaption>
+                    </a>
+                </div>
+
+            </div>
+
+        </div>
+    )
+}
